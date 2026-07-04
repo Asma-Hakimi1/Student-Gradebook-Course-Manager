d69180ae4a8b3b397460802d27a2167d3ef38536
AsmaHakimi <movahedhakimi404577@gmail.com>
2026-07-04 20:15:58 +0430

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,20 @@
+from assessment import Assessment
 
+
+class Project(Assessment):
+
+    def __init__(self, title, max_score):
+        super().__init__(title, max_score)
+
+    def display_info(self):
+        print(f"Project: {self.title} - Max Score: {self.max_score}")
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+
+        if percentage >= 90:
+            return "Excellent project."
+        elif percentage >= 55:
+            return "Project submitted."
+        else:
+            return "Project needs improvement."
