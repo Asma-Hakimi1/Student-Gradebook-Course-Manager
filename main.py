35b44d20143be066ae142b0898bf4b4dd046fa1d
AsmaHakimi <movahedhakimi404577@gmail.com>
2026-07-05 17:23:22 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from student import Student
+from course import Course
+from quiz import Quiz
+from exam import Exam
+from project import Project
+from gradebook import Gradebook
 
+gradebook = Gradebook()
