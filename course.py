74fb7808842199d0f0e44313b6a848c5e512d8c5
AsmaHakimi <movahedhakimi404577@gmail.com>
2026-07-05 15:33:36 +0430

    Update course.py

--- course.py
+++ course.py
@@ -4,3 +4,33 @@ class Course:
         self.course_name = course_name
         self.students = []
         self.assessments = []
+
+def add_student(self, student_id):
+        if student_id not in self.students:
+            self.students.append(student_id)
+            print("Student enrolled successfully.")
+        else:
+            print("Student is already enrolled.")
+
+def add_assessment(self, assessment):
+        self.assessments.append(assessment)
+        print("Assessment added successfully.")
+
+def find_assessment(self, title):
+        for assessment in self.assessments:
+            if assessment.title == title:
+                return assessment
+        return None
+
+def display_info(self):
+        print("\n===== Course Information =====")
+        print("Course Code:", self.course_code)
+        print("Course Name:", self.course_name)
+        print("Enrolled Students:", len(self.students))
+
+        if self.assessments:
+            print("Assessments:")
+            for assessment in self.assessments:
+                assessment.display_info()
+        else:
+            print("No assessments added.")
