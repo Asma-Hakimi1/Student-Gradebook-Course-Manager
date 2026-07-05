6b881fc635188d01c548176d6051379a14ea4fa2
AsmaHakimi <movahedhakimi404577@gmail.com>
2026-07-05 17:31:30 +0430

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ from project import Project
 from gradebook import Gradebook
 
 gradebook = Gradebook()
+
+while True:
+
+    print("\n===== Student Gradebook Manager =====")
+    print("1. Add Student")
+
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+
+        student_id = input("Student ID: ")
+        name = input("Name: ")
+        email = input("Email: ")
+
+        student = Student(student_id, name, email)
+        gradebook.add_student(student)
