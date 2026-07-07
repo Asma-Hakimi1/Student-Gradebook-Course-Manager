0a67cd87ca5f3094aef5e5fca47f92d1fb27f021
AsmaHakimi <movahedhakimi404577@gmail.com>
2026-07-07 19:43:55 +0430

    Update assessment.py

--- assessment.py
+++ assessment.py
@@ -2,3 +2,21 @@ class Assessment:
     def __init__(self, title, max_score):
         self.title = title
         self.max_score = max_score
+
+    def calculate_percentage(self, score):
+        return (score / self.max_score) * 100
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+
+        if percentage >= 80:
+            return "Excellent"
+        elif percentage >= 60:
+            return "Good"
+        elif percentage >= 50:
+            return "Passed"
+        else:
+            return "Failed"
+
+    def display_info(self):
+        print(f"{self.title} - Max Score: {self.max_score}")
